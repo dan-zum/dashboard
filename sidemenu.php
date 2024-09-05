3db6f414ca9320281e3fc5e9a008def974c7d094
Stardust <aalonetecciztecatl@gmail.com>
2024-09-05 13:44:52 -0600

    cambios del menu

---
+++ sidemenu.php
@@ -0,0 +1,40 @@
+<!-- menu.php -->
+<div class="sidebar-menu">
+  <div class="sidebarMenuScroll">
+    <ul>
+      <li class="sidebar-dropdown active">
+        <a href="#">
+          <i class="bi bi-house"></i>
+          <span class="menu-text">Panel de control</span>
+        </a>
+        <div class="sidebar-submenu">
+          <ul>
+            <li>
+              <a href="index.html" class="current-page">Nuevo Pedido</a>
+            </li>
+            <li>
+              <a href="reports.html">Historial Pedidos</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="sidebar-dropdown">
+        <a href="#">
+          <i class="bi bi-handbag"></i>
+          <span class="menu-text">Mi stocks</span>
+        </a>
+        <div class="sidebar-submenu">
+          <ul>
+            <li>
+              <a href="orders.html">My stock</a>
+            </li>
+            <li>
+              <a href="products.html">Comprar stock</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>
+<!-- Sidebar menu ends -->
